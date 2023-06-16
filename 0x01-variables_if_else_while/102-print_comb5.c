f9b11c33c5696576560322d67a8c86fb93862be5
Sm-Abla <ablasmina1996@gmail.com>
2023-06-16 14:31:28 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,7 +13,7 @@ for (j = 0; j < 100; j++)
 if (i < j)
 {
 putchar((i / 10) + 48);
-putchar((i % 10) + c48);
+putchar((i % 10) + 48);
 putchar(' ');
 putchar((j / 10) + 48);
 putchar((j % 10) + 48);
