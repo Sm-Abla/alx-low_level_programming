cf0f3989359a270405dbf7c5284dd180120f8c58
Sm-Abla <ablasmina1996@gmail.com>
2023-06-22 09:24:19 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print number - Print the number since 0 up to 9
+ * Return: The numbers since 0 up to 9
+*/
+
+void print_number(void)
+
+{
+int x;
+for (x = 0; x <= 9; x++)
+{
+_putchar(x + '0');
+}
+_putchar('\n');
+}
